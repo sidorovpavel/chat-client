0a50225cb0de5b27c92330fa340e8ab60d9e5095
Pavel Sidorov <sidorovpav@ya.ru>
2019-06-30 18:36:17 +0300

    Updates

---
+++ precache-manifest.84f31b2a8b1fd7158cd9fd1abecb8a16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28522421cd0c017471c8582fd395e2a9",
+    "url": "/chat-client/index.html"
+  },
+  {
+    "revision": "7b0ada60e5adb4821d3d",
+    "url": "/chat-client/static/css/main.2d752cab.chunk.css"
+  },
+  {
+    "revision": "027681601cbab20ec5b9",
+    "url": "/chat-client/static/js/2.546bac3d.chunk.js"
+  },
+  {
+    "revision": "7b0ada60e5adb4821d3d",
+    "url": "/chat-client/static/js/main.fcc54873.chunk.js"
+  },
+  {
+    "revision": "fffd2439cce897adae5e",
+    "url": "/chat-client/static/js/runtime~main.da75766a.js"
+  }
+]);
